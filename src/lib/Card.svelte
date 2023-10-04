652372073302c753163328c181d49c56a6e8c17e
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-04 11:39:59 +0300

    Init

---
+++ src/lib/Card.svelte
@@ -0,0 +1,46 @@
+<script>
+export let date, title, price, note,status;
+$ : IsComplete = status == 3;
+</script>
+
+<div>
+    <div class="card m-3 p-3">
+        <div >
+            <span  class="text-dark">
+                {date}
+            </span>
+            <h1 class:middle-stroke={IsComplete} class:disabled={IsComplete}  class="text-primary fw-bolder">
+                {title}
+            </h1>
+        </div>
+        <div >
+            <p class:middle-stroke={IsComplete} class:disabled={IsComplete}  class="text-dark fs-3">
+                {note}
+            </p>
+        </div>
+        <div  >
+            <div class="row">
+                {#if !IsComplete }
+                <div class="col-9">       
+                             <button class="btn btn-lg btn-secondary" >Mark As Completed</button>  
+                   
+                      <button class="ms-2 btn btn-lg btn-outline-dark" >open Note</button>  
+          </div>
+                <div class="col-3 text-end"><h2 class="text-secondary">{price} EGP</h2></div>
+                {:else}
+
+                <div class="col-9">       
+                    <button class="btn btn-lg btn-primary disabled" >Undo Note</button>  
+        
+        <button class="ms-2 btn btn-lg btn-outline-dark disabled" >open Note</button>  
+    </div>
+ 
+       <div class="col-3 text-end" ><h2 class="text-secondary disabled">{price} EGP</h2></div>
+
+              {/if}
+            </div>
+           
+        </div>
+    </div>
+</div>
+ 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
-    import { enhance } from "$app/forms";
+import Card from "$lib/Card.svelte";
+import { enhance } from "$app/forms";
     export let data;
 </script>
-<form method="POST" action="?/create" use:enhance>
+
+<Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="3" />
+<Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="2" />
+<Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="1" />
+
+<!-- <form method="POST" action="?/create" use:enhance>
     <input type="text" name="title" placeholder="title" />
     <input type="number" name="price" placeholder="price" />
     <input type="text" name="note" placeholder="note" />
@@ -21,4 +27,4 @@
             <td>{item.Note}</td>
         </tr>
     {/each}
-</table>
+</table> -->
