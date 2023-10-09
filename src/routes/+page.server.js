c5b33c25fc231da2927831499b65a7d67c285528
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-10 01:11:17 +0300

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,47 +2,47 @@ import { sql } from "@vercel/postgres";
 
 export async function load() {
     return {
-        cart: await sql`SELECT * from CART `
+        cart: await sql`SELECT * from ShoppingList.CART `
     }
 }
 
 export const actions = {
     create: async ({ cookies, request }) => {
         const { title, price, note } = Object.fromEntries(await request.formData());
-        await sql`INSERT INTO CART (Title, Price, Note, Status, Date) VALUES (${title}, ${price}, ${note}, 0, to_char(now(), 'YYYY-MM-DD'))`;
+        await sql`INSERT INTO ShoppingList.CART (Title, Price, Note, Status, Date) VALUES (${title}, ${price}, ${note}, 0, to_char(now(), 'YYYY-MM-DD'))`;
         return {
-            cart: await sql`SELECT * from CART `
+            cart: await sql`SELECT * from ShoppingList.CART `
         }
     },
     update: async ({ params, request }) => {
         const { title, price, note } = Object.fromEntries(await request.formData());
-        await sql`UPDATE CART SET Title = ${title}, Price = ${price}, Note = ${note} WHERE ID = ${params.id}`;
+        await sql`UPDATE ShoppingList.CART SET Title = ${title}, Price = ${price}, Note = ${note} WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from CART `
+            cart: await sql`SELECT * from ShoppingList.CART `
         }
     },
     delete: async ({ params }) => {
-        await sql`DELETE FROM CART WHERE ID = ${params.id}`;
+        await sql`DELETE FROM ShoppingList.CART WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from CART `
+            cart: await sql`SELECT * from ShoppingList.CART `
         }
     },
     reset: async ({ params }) => {
-        await sql`UPDATE CART SET Status = 0 WHERE ID = ${params.id}`;
+        await sql`UPDATE ShoppingList.CART SET Status = 0 WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from CART `
+            cart: await sql`SELECT * from ShoppingList.CART `
         }
     },
     complete: async ({ params }) => {
-        await sql`UPDATE CART SET Status = 1 WHERE ID = ${params.id}`;
+        await sql`UPDATE ShoppingList.CART SET Status = 1 WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from CART `
+            cart: await sql`SELECT * from ShoppingList.CART `
         }
     },
     pin: async ({ params }) => {
-        await sql`UPDATE CART SET Status = 2 WHERE ID = ${params.id}`;
+        await sql`UPDATE ShoppingList.CART SET Status = 2 WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from CART `
+            cart: await sql`SELECT * from ShoppingList.CART `
         }
     }
 }
