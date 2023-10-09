76f78a8420e157841cbc078e8f797ccf61cde973
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-10 01:57:13 +0300

    test

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,48 +1,49 @@
 import { sql } from "@vercel/postgres";
 
+ 
 export async function load() {
     return {
-        cart: await sql`SELECT * from ShoppingList.CART `
+        cart: await sql`SELECT * from CART`
     }
 }
 
 export const actions = {
     create: async ({ cookies, request }) => {
         const { title, price, note } = Object.fromEntries(await request.formData());
-        await sql`INSERT INTO ShoppingList.CART (Title, Price, Note, Status, Date) VALUES (${title}, ${price}, ${note}, 0, to_char(now(), 'YYYY-MM-DD'))`;
+        await sql`INSERT INTO CART (Title, Price, Note, Status, Date) VALUES (${title}, ${price}, ${note}, 0, to_char(now(), 'YYYY-MM-DD'))`;
         return {
-            cart: await sql`SELECT * from ShoppingList.CART `
+            cart: await sql`SELECT * from CART `
         }
     },
     update: async ({ params, request }) => {
         const { title, price, note } = Object.fromEntries(await request.formData());
-        await sql`UPDATE ShoppingList.CART SET Title = ${title}, Price = ${price}, Note = ${note} WHERE ID = ${params.id}`;
+        await sql`UPDATE CART SET Title = ${title}, Price = ${price}, Note = ${note} WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from ShoppingList.CART `
+            cart: await sql`SELECT * from CART `
         }
     },
     delete: async ({ params }) => {
-        await sql`DELETE FROM ShoppingList.CART WHERE ID = ${params.id}`;
+        await sql`DELETE FROM CART WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from ShoppingList.CART `
+            cart: await sql`SELECT * from CART `
         }
     },
     reset: async ({ params }) => {
-        await sql`UPDATE ShoppingList.CART SET Status = 0 WHERE ID = ${params.id}`;
+        await sql`UPDATE CART SET Status = 0 WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from ShoppingList.CART `
+            cart: await sql`SELECT * from CART `
         }
     },
     complete: async ({ params }) => {
-        await sql`UPDATE ShoppingList.CART SET Status = 1 WHERE ID = ${params.id}`;
+        await sql`UPDATE CART SET Status = 1 WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from ShoppingList.CART `
+            cart: await sql`SELECT * from CART `
         }
     },
     pin: async ({ params }) => {
-        await sql`UPDATE ShoppingList.CART SET Status = 2 WHERE ID = ${params.id}`;
+        await sql`UPDATE CART SET Status = 2 WHERE ID = ${params.id}`;
         return {
-            cart: await sql`SELECT * from ShoppingList.CART `
+            cart: await sql`SELECT * from CART `
         }
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,27 +4,33 @@ import { enhance } from "$app/forms";
     export let data;
 </script>
 
-<Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="3" />
+<!-- <Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="3" />
 <Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="2" />
-<Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="1" />
+<Card date="2021-10-10 - 1:11 PM" title="I Want To Buy A Caple For My Laptop" price="100" note="here is some dummy text" status="1" /> -->
 
-<!-- <form method="POST" action="?/create" use:enhance>
-    <input type="text" name="title" placeholder="title" />
-    <input type="number" name="price" placeholder="price" />
-    <input type="text" name="note" placeholder="note" />
-    <button type="submit">Submit</button>
-</form>
+ <form method="POST" action="?/create" use:enhance>
+<input type="text" name="title" placeholder="title" />
+<input type="number" name="price" placeholder="price" />
+<input type="text" name="note" placeholder="note" />
+<button type="submit">Submit</button>
+</form> 
 <table class="table">
     <tr>
+        <th>ID</th>
         <th>Title</th>
         <th>Price</th>
-        <th>Note</th>
+        <th>Status</th>
+        <th>Date</th>
+         
     </tr>
-    {#each data.message as item}
+    {#each data.cart as item}
         <tr>
+            <td>{item.ID}</td>
             <td>{item.Title}</td>
             <td>{item.Price}</td>
-            <td>{item.Note}</td>
+            <td>{item.Status}</td>
+            <td>{item.Date}</td>
+  
         </tr>
     {/each}
-</table> -->
+</table> 
