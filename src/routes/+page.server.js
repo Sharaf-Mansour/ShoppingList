4c0fe21303b8d9ab5ef1cd6d0a13f69f8f481921
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-10 00:38:07 +0300

    init

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,16 +1,48 @@
-import { Database } from "bun:sqlite";
-const db = new Database("static/MainDB.db");
+import { sql } from "@vercel/postgres";
 
-export const load = async () => {
-    const query = db.query(`SELECT * from ListTable`);
-    const result = query.all();
-    return { message: result }
+export async function load() {
+    return {
+        cart: await sql`SELECT * from CART `
+    }
 }
 
 export const actions = {
     create: async ({ cookies, request }) => {
-        const {title,price,note} = Object.fromEntries(await request.formData());
-        const query = db.prepare('INSERT INTO ListTable (Title, Price, Note) VALUES ($title, $price, $note)');
-        query.run(title, price, note);
+        const { title, price, note } = Object.fromEntries(await request.formData());
+        await sql`INSERT INTO CART (Title, Price, Note, Status, Date) VALUES (${title}, ${price}, ${note}, 0, to_char(now(), 'YYYY-MM-DD'))`;
+        return {
+            cart: await sql`SELECT * from CART `
+        }
+    },
+    update: async ({ params, request }) => {
+        const { title, price, note } = Object.fromEntries(await request.formData());
+        await sql`UPDATE CART SET Title = ${title}, Price = ${price}, Note = ${note} WHERE ID = ${params.id}`;
+        return {
+            cart: await sql`SELECT * from CART `
+        }
+    },
+    delete: async ({ params }) => {
+        await sql`DELETE FROM CART WHERE ID = ${params.id}`;
+        return {
+            cart: await sql`SELECT * from CART `
+        }
+    },
+    reset: async ({ params }) => {
+        await sql`UPDATE CART SET Status = 0 WHERE ID = ${params.id}`;
+        return {
+            cart: await sql`SELECT * from CART `
+        }
+    },
+    complete: async ({ params }) => {
+        await sql`UPDATE CART SET Status = 1 WHERE ID = ${params.id}`;
+        return {
+            cart: await sql`SELECT * from CART `
+        }
+    },
+    pin: async ({ params }) => {
+        await sql`UPDATE CART SET Status = 2 WHERE ID = ${params.id}`;
+        return {
+            cart: await sql`SELECT * from CART `
+        }
     }
 }
