009842805e212c4bbaada19f3b64887c91d9b960
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-10 20:48:19 +0300

    Fix card design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,19 @@
 import Card from '$lib/Card.svelte';
 import { enhance } from "$app/forms";
 export let data;
+$: array = data.cart.sort((b, a) => a.status - b.status);
 let date = new Date().toLocaleDateString();
 </script>
- 
-  <form method="POST" action="?/create" use:enhance>
+<form method="POST" action="?/create" use:enhance>
 <input type="text" name="title" placeholder="title" />
 <input type="number" name="price" placeholder="price" />
 <input type="text" name="note" placeholder="note" />
 <button type="submit">Submit</button>
 </form> 
-{#each data.cart as item }
+<div class="row m-2">
+
+
+{#each array as item (item.id)}
 <Card {date} id={item.id} title={item.title} price={item.price} note={item.note} status={item.status} />
 {/each}
+</div>
