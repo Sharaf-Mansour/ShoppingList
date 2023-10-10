d1ab872a06045be5304cc3480a4c11a58c036192
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-10 04:09:38 +0300

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -16,39 +16,27 @@ export const actions = {
     create: async ({ cookies, request }) => {
         const { title, price, note } = Object.fromEntries(await request.formData());
         await sql`INSERT INTO CART (Title, Price, Note, Status, add_date) VALUES (${title}, ${price}, ${note}, 0, now())`;
-        return {
-            cart: GetCart()
-        }
+     
     },
     update: async ({ params, request }) => {
         const { title, price, note } = Object.fromEntries(await request.formData());
         await sql`UPDATE CART SET Title = ${title}, Price = ${price}, Note = ${note} WHERE ID = ${params.id}`;
-        return {
-            cart: GetCart()
-        }
+        
     },
     delete: async ({ params }) => {
         await sql`DELETE FROM CART WHERE ID = ${params.id}`;
-        return {
-            cart: GetCart()
-        }
+       
     },
     reset: async ({ params }) => {
         await sql`UPDATE CART SET Status = 0 WHERE ID = ${params.id}`;
-        return {
-            cart: GetCart()
-        }
+   
     },
     complete: async ({ params }) => {
         await sql`UPDATE CART SET Status = 1 WHERE ID = ${params.id}`;
-        return {
-            cart: GetCart()
-        }
+      
     },
     pin: async ({ params }) => {
         await sql`UPDATE CART SET Status = 2 WHERE ID = ${params.id}`;
-        return {
-            cart: GetCart()
-        }
+       
     }
 }
