70a8b8bcc35d520b0f8c294d2e7b86dd8bc3a2c9
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-14 03:22:23 +0300

    Add Stores for Update and Delete

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,10 +1,20 @@
 import { writable } from 'svelte/store';
-export let show = writable(false);
 export let cart = writable({});
+export let show = writable(false);
 export let showCreate = writable(false);
-export const setShow = () => show.update(value => !value);
+export let showUpdate = writable(false);
+export let showDelete = writable(false);
 export const setCart = (x) => {
     cart.update(value => value = x);
     show.update(value => value = true)
 }
+export const setShow = () => show.update(value => !value);
 export const setShowCreate = () => showCreate.update(value => !value);
+export const setShowUpdate = () => {
+    show.update(value => !value);
+    showUpdate.update(value => !value);
+}
+export const setShowDelete = () => {
+    showUpdate.update(value => !value)
+    showDelete.update(value => !value);
+}
